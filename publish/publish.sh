0bca145c876a98c2e0521ef17b9e27e7fe6592a9
Ihor S <siafin2010@gmail.com>
2025-06-16 15:12:16 +0200

    fix

--- publish/publish.sh
+++ publish/publish.sh
@@ -1,6 +1,6 @@
 git add . --all
-git commit -am"publish new version $@"
 VERSION=$(npm run version --silent)
+git commit -am"publish new version $VERSION"
 git tag $VERSION -m "new version"
 git push
 git push --tags
